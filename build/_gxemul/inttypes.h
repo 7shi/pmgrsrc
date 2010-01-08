375bc6ce2ca06b17a5480f2a45bff62d1cb4a6f6
7shi <7shi@live.jp>
2010-01-08 20:54:36 +0900

    netbsd-0.3-20091229

--- build/_gxemul/inttypes.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STDINT_H__
-#define __STDINT_H__
-
-#include <sys/types.h>
-typedef u_int64_t uint64_t;
-
-#endif
--- common.mk
+++ common.mk
@@ -7,6 +7,8 @@ EXTRACT     = pmgr-extract
 PKGCOMPRESS = xz
 DISTDIR     = $(TOPDIR)/distfiles
 PKGDIR      = $(TOPDIR)/packages
+PKGCPPFLAGS = -I$(PKGDEST)/include -I/usr/pkg/include
+PKGLDFLAGS  = -L$(PKGDEST)/lib -L/usr/pkg/lib -Wl,-R$(PKGDEST)/lib -Wl,-R/usr/pkg/lib
 COMMANDS    = all fetch extract config build \
               package repackage install reinstall uninstall \
               clean distclean
